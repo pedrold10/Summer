9c265284f682ced65036000863929808cf2717b9
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2022-10-15 10:22:09 -0300

    Adicionando DAO e Helper

---
+++ app/src/main/java/com/example/summer/BancoHelper.kt
@@ -0,0 +1,18 @@
+package com.example.summer
+
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BancoHelper (var context: Context): SQLiteOpenHelper(context, "ifcrud.db", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val sql = "create table pessoas(id integer primary key autoincrement, nome text, data integer)"
+        db?.execSQL(sql)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, anterior: Int, atual: Int) {
+
+    }
+}
